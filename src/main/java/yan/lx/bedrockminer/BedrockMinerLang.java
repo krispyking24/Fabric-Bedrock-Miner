29d28bd24e2ad57c8e1302ea2527fb304b8b9dfd
Bunny_i <1160706050@qq.com>
2023-06-27 18:19:37 +0800

    添加上一个提交漏掉的新类

---
+++ src/main/java/yan/lx/bedrockminer/BedrockMinerLang.java
@@ -0,0 +1,22 @@
+package yan.lx.bedrockminer;
+
+import net.minecraft.text.Text;
+
+public class BedrockMinerLang {
+    public static final Text TOGGLE_ON = Text.translatable("bedrockminer.toggle.on");
+    public static final Text TOGGLE_OFF = Text.translatable("bedrockminer.toggle.off");
+
+    public static final Text DEBUG_ON = Text.translatable("bedrockminer.debug.on");
+    public static final Text DEBUG_OFF = Text.translatable("bedrockminer.debug.off");
+
+
+    public static final Text WARN_MULTIPLAYER = Text.translatable("bedrockminer.warn.multiplayer");
+    public static final Text FAIL_MISSING_SURVIVAL = Text.translatable("bedrockminer.fail.missing.survival");
+    public static final Text FAIL_MISSING_PISTON = Text.translatable("bedrockminer.fail.missing.piston");
+    public static final Text FAIL_MISSING_REDSTONETORCH = Text.translatable("bedrockminer.fail.missing.redstonetorch");
+    public static final Text FAIL_MISSING_SLIME = Text.translatable("bedrockminer.fail.missing.slime");
+    public static final Text FAIL_MISSING_INSTANTMINE = Text.translatable("bedrockminer.fail.missing.instantmine");
+
+    public static final Text FAIL_PLACE_PISTON = Text.translatable("bedrockminer.fail.place.piston");
+    public static final Text FAIL_PLACE_SLIMEBLOCK = Text.translatable("bedrockminer.fail.place.slimeBlock");
+}
