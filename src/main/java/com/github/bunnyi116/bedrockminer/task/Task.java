4b7d9a8850f9583ad682b7da3977359ee6d87fd1
bunnyi116 <1160706050@qq.com>
2025-11-14 05:54:56 +0800

    fix

--- src/main/java/com/github/bunnyi116/bedrockminer/task/Task.java
+++ src/main/java/com/github/bunnyi116/bedrockminer/task/Task.java
@@ -390,7 +390,7 @@ public class Task {
             this.executed = true;
         }
         if (Config.getInstance().taskShort) {
-            this.currentState = TaskState.WAIT_GAME_UPDATE;
+            this.setWait(TaskState.WAIT_GAME_UPDATE, 1);
         } else {
             this.setWait(TaskState.WAIT_GAME_UPDATE, 3);
         }
